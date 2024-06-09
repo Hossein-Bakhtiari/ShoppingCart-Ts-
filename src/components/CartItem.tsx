e99b76adafe247b9a21f01285625ca71ea3e7bbb
hossein <hosseinbakhtiari781@outlook.com>
2024-06-09 18:34:16 +0330

    final changes

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,38 +1,42 @@
-import { Button, Stack } from "react-bootstrap"
-import { useShoppingCart } from "../context/ShopipngCartContext"
-import storeItems from "../data/itemes.json"
-import { formatCurrency } from "../utilities/cormatCurrency"
+import { Button, Stack } from "react-bootstrap";
+import { useShoppingCart } from "../context/ShopipngCartContext";
+import storeItems from "../data/itemes.json";
+import { formatCurrency } from "../utilities/cormatCurrency";
 
 type cartItemProps = {
-    id: number
-    quantity: number
-}
-export function CartItem({id, quantity}: cartItemProps) {
-    const { removeFromeCart } = useShoppingCart()
-    const item = storeItems.find(i => i.id === id)
-    if(item == null) return null
-    return(
-        <Stack direction="horizontal" gap={2} className="d-flex
-        align-items-center">
-            <img
-                src={item.imgUrl} style={{width: "125px", 
-                height:"75px" , objectFit:"cover" }}   
-            />
-            <div className="me-auto">
-                <div>
-                    {item.name}{" "}
-                    {quantity > 1 && (
-                        <span className="text-muted" style={{ fontSize: ".65rem" }}>
-                            x{quantity}
-                        </span>
-                    )}
-                </div>
-            </div>
+  id: number;
+  quantity: number;
+};
+export function CartItem({ id, quantity }: cartItemProps) {
+  const { removeFromeCart } = useShoppingCart();
+  const item = storeItems.find((i) => i.id === id);
+  if (item == null) return null;
+  return (
+    <Stack
+      direction="horizontal"
+      gap={2}
+      className="d-flex
+        align-items-center"
+    >
+      <img
+        src={item.imgUrl}
+        style={{ width: "125px", height: "75px", objectFit: "cover" }}
+      />
+      <div className="me-auto">
+        <div>
+          {item.name}{" "}
+          {quantity > 1 && (
+            <span className="text-muted" style={{ fontSize: ".65rem" }}>
+              x{quantity}
+            </span>
+          )}
+        </div>
+      </div>
 
-            <div className="text-muted" style={{ fontSize: ".75rem" }}>
-                {formatCurrency(item.price)}
-            </div>
-            <div> {formatCurrency(item.price * quantity)}</div>
+      <div className="text-muted" style={{ fontSize: ".75rem" }}>
+        {formatCurrency(item.price)}
+      </div>
+      <div> {formatCurrency(item.price * quantity)}</div>
       <Button
         variant="outline-danger"
         size="sm"
@@ -40,8 +44,6 @@ export function CartItem({id, quantity}: cartItemProps) {
       >
         &times;
       </Button>
-            
-        </Stack>
-    )
+    </Stack>
+  );
 }
-
--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -2,35 +2,35 @@ import { Offcanvas, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/ShopipngCartContext";
 import { CartItem } from "./CartItem";
 import { formatCurrency } from "../utilities/cormatCurrency";
-import  storeItems  from "../data/itemes.json";
+import storeItems from "../data/itemes.json";
 
 type ShoppingCartProps = {
-    isOpen: boolean
-}
+  isOpen: boolean;
+};
 
-export function ShoppingCart({isOpen}: ShoppingCartProps){
-    const {closeCart , cartItems} = useShoppingCart();
-    return (
-        <Offcanvas show={isOpen} onHide={closeCart} placement="end">
-            <Offcanvas.Header closeButton>
-                <Offcanvas.Title>Cart</Offcanvas.Title>
-            </Offcanvas.Header>
-            <Offcanvas.Body>
-                <Stack gap={3}>
-                    {cartItems.map(item => (
-                        <CartItem key={item.id} {...item} />
-                    ) )}
+export function ShoppingCart({ isOpen }: ShoppingCartProps) {
+  const { closeCart, cartItems } = useShoppingCart();
+  return (
+    <Offcanvas show={isOpen} onHide={closeCart} placement="end">
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title>Cart</Offcanvas.Title>
+      </Offcanvas.Header>
+      <Offcanvas.Body>
+        <Stack gap={3}>
+          {cartItems.map((item) => (
+            <CartItem key={item.id} {...item} />
+          ))}
           <div className="ms-auto fw-bold fs-5">
             Total{" "}
             {formatCurrency(
               cartItems.reduce((total, cartItem) => {
-                const item = storeItems.find(i => i.id === cartItem.id)
-                return total + (item?.price || 0) * cartItem.quantity
+                const item = storeItems.find((i) => i.id === cartItem.id);
+                return total + (item?.price || 0) * cartItem.quantity;
               }, 0)
             )}
           </div>
-                </Stack>
-            </Offcanvas.Body>
-         </Offcanvas>
-    )
+        </Stack>
+      </Offcanvas.Body>
+    </Offcanvas>
+  );
 }
